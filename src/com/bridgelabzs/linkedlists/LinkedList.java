cc052ac45bbe6db09656196a0c83d4c3b7b9185d
Sreedhar2484 <nagallisreedhar1918@gmail.com>
2023-02-03 17:06:55 +0530

    refactor: Program to sort the linked list in ascending order

--- src/com/bridgelabzs/linkedlists/LinkedList.java
+++ src/com/bridgelabzs/linkedlists/LinkedList.java
@@ -1,4 +1,5 @@
 package com.bridgelabzs.linkedlists;
+import java.util.Scanner;
 
 public class LinkedList{
     Node head;
@@ -42,97 +43,52 @@ public class LinkedList{
             temp.ref = newNode;
         }
         }
-
-        public void search(Object searchElement) {
-            if (head.data == searchElement)
-                System.out.println(searchElement + " is Found");
-            else {
-                Node temp = head;
-
-                boolean isFound = false;
-
-                while (temp != null) {
-                    if (temp.data == searchElement) {
-                        isFound = true;
-                        break;
-                    }
-
-                    temp = temp.ref;
-                }
-
-                if (isFound == true)
-                    System.out.println(searchElement + " is Found");
-                else
-                    System.out.println(searchElement + " is not found..");
-                }
-            }
-
-        void insert( int position,Object data) {  
-            Node newNode = new Node(data); 
-            newNode.ref = null;
-            if(position < 1) {
-                System.out.print("\nposition should be >= 1.");
-            } else if (position == 1) {
-                newNode.ref = head;
-                head = newNode;
-            } else {
-                Node temp = new Node(data);
-                temp = head;
-                for(int i = 1; i < position-1; i++) {
-                    if(temp != null) {
-                        temp = temp.ref;
-                    }
-                }
-                if(temp != null) {
-                    newNode.ref = temp.ref;
-                    temp.ref = newNode;  
-                } else {
-                    System.out.print("\nThe previous node is null.");
-                }       
-            }
-        } 
-        
-        public void delete_Data(Object data) {
-            int index = 0;
-            Node left = head;
-            Node right = left.ref;
-            while (right.data != data) {
-                left = left.ref;
-                right = right.ref;
-                index++;
-
-            }
-            left.ref = right.ref;
-        }
-        
-        public void size(){
-            Node temp=head;
-            int count = 0;
-            while(temp!=null)
-            {
-                temp=temp.ref;
-                count++;
-            }  
-            System.out.println("Size of Linked List is : " + count);
-            //return count;
-        }
-            
     
+    public void sortList() {
+            // Node current will point to head
+            Node current = head, index = null;
+            int temp;
+
+            if (head == null) {
+                return;
+            } else {
+                while (current != null) {
+                    // Node index will point to node next to current
+                    index = current.ref;
+
+                    while (index != null) {
+                        // If current node's data is greater than index's node data, swap the data
+                        // between them
+                        if (current.data > index.data) {
+                            temp = current.data;
+                            current.data = index.data;
+                            index.data = temp;
+                        }
+                        index = index.ref;
+                    }
+                    current = current.ref;
+                }
+            }
+        }
+    
+        
+
+        
+        
     public static void main(String args[]){
         LinkedList LL1 = new LinkedList();
         LL1.append(56);
         LL1.display();
         LL1.append(30);
         LL1.display();
+        LL1.append(40);
+        LL1.display();
         LL1.append(70);
         LL1.display();
-        LL1.search(30);
-        LL1.insert(3, 40);
+        LL1.sortList();
         LL1.display();
-        LL1.search(40);
-        LL1.delete_Data(40);
-        LL1.size();
-        LL1.display();
+        
+        
         
         
     }
